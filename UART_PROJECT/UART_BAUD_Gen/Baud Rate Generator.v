cdac56e55d904781826fe6c4190b2756a13f59f6
Vishalpalanisamy <vishal.nkl04@gmail.com>
2026-01-26 16:36:31 +0530

    Create Baud Rate Generator.v

---
+++ UART_PROJECT/UART_BAUD_Gen/Baud Rate Generator.v
@@ -0,0 +1,27 @@
+module uart_baud #
+(
+    parameter CLKS_PER_BIT = 108
+)
+(
+    input         clk,
+    input         rst,
+    output reg    tick
+);
+
+    reg [$clog2(CLKS_PER_BIT)-1:0] counter;
+
+    always @(posedge clk or posedge rst) begin
+        if (rst) begin
+            counter <= 0;
+            tick    <= 0;
+        end else begin
+            if (counter == CLKS_PER_BIT-1) begin
+                counter <= 0;
+                tick    <= 1;
+            end else begin
+                counter <= counter + 1;
+                tick    <= 0;
+            end
+        end
+    end
+endmodule
